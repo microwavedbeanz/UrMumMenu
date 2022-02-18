45e1ab3c114c6a0b66f78f53d3919e8378ada315
Josh <92121228+OtterzMan@users.noreply.github.com>
2022-02-18 14:07:06 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,328 @@
+#imports
+import click
+import time
+import random
+import ctypes
+import colorama
+import os
+import json
+import subprocess
+import webbrowser
+
+#Gilp Locator
+gilp = ("\Program Files (x86)\GILP 1.3 x32\GILP.exe")
+
+#from imports
+from colorama import Style, Fore, init
+
+#title set
+ctypes.windll.kernel32.SetConsoleTitleW("Ur mum ┇ Best program - ccp 2021")
+
+#cls
+def cls():
+    click.clear()
+
+#header
+print('''
+                                                                
+                                                                
+    //   / /                                                    
+   //   / /      __          _   __                   _   __    
+  //   / /     //  ) )     // ) )  ) )   //   / /   // ) )  ) ) 
+ //   / /     //          // / /  / /   //   / /   // / /  / /  
+((___/ /     //          // / /  / /   ((___( (   // / /  / /   
+---------------------------------------------------------------
+MADE BY JOSH ON A WEDNESDAY
+---------------------------------------------------------------
+''')
+# menu 
+print (30 * '-')
+print ("   M A I N - M E N U")
+print (30 * '-')
+print ("[1] die")
+print ("[2] Open GILP")
+print ("[3] Get Good")
+print ("[4] Britney ?!")
+print ("[5] I need maths help [!!!]")
+print ("[6] DO NOT CHOOSE THIS PLEASE [!!!!]")
+print ("[7] There is no option")
+print ("[8] FREE ROBUX[!!!!!!!!!!!!]")
+print ("[9] Discord Mod")
+print ("[10] [!!!!!!!!!!!!]")
+print (30 * '-')
+ 
+#Input Getter
+choice = input('Enter your choice [1-10] : ')
+ 
+### Convert string to int type ##
+choice = int(choice)
+ 
+#Choices
+if choice == 1:
+    cls()
+    print ("dieing in 3...")
+    time.sleep(1)
+    cls()
+    print ("dieing in 2...")
+    time.sleep(1)
+    cls()
+    print ("dieing in 1...")
+    time.sleep(1)
+    exit()
+elif choice == 2:
+    cls()
+    print ("OPENING GILP...")
+    time.sleep(3)
+    os.startfile(gilp)
+elif choice == 3:
+    cls()
+    print("Hacking China.")
+    time.sleep(0.5)
+    cls()
+    print("Hacking China..")
+    time.sleep(0.5)
+    cls()
+    print("Hacking China.")
+    time.sleep(0.5)
+    cls()
+    print("DONE !")
+    time.sleep(1.5)
+    webbrowser.open('https://www.gov.cn/english/')
+    time.sleep(3)
+    exit()
+
+elif choice == 4:
+    cls()
+    print("Hacking Britney.")
+    time.sleep(0.5)
+    cls()
+    print("Hacking Britney..")
+    time.sleep(0.5)
+    cls()
+    print("Hacking Britney...")
+    time.sleep(0.5)
+    cls()
+    print("DONE !")
+    time.sleep(1.5)
+    webbrowser.open('https://www.youtube.com/watch?v=C-u5WLJ9Yk4&list=PLE280C7E3DAA996E9')
+    time.sleep(3)
+    exit()
+elif choice == 5:
+    cls()
+    print("What have you done")
+    time.sleep(3)
+    os.startfile("calc.exe")
+    time.sleep(5)
+    print("Thats not it hahahahaha")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    os.startfile("calc.exe")
+    print("kys please")
+    exit()
+
+elif choice == 6:
+    cls()
+    print("PLS NO")
+    time.sleep(1.5)
+    cls()
+    print("WHY !!!!!!!!!!!!!!")
+    time.sleep(1.5)
+    cls()
+    print("""
+##  ###  ##  ##    ## ##   
+##  ##   ##  ##   ##   ##  
+## ##    ##  ##   ####     
+## ##     ## ##    #####   
+## ###     ##         ###  
+##  ##     ##     ##   ##  
+##  ###    ##      ## ##   
+                           
+""")
+    time.sleep(5)
+    webbrowser.open('https://www.youtube.com/watch?v=h-GWttKZUoo')
+    exit()
+
+elif choice == 7:
+    cls()
+    print("""
+░░░░░░░░░▄██████████▄▄░░░░░░░░
+░░░░░░▄█████████████████▄░░░░░
+░░░░░██▀▀▀▀▀▀▀▀▀▀▀████████░░░░
+░░░░██░░░░░░░░░░░░░░███████░░░
+░░░██░░░░░░░░░░░░░░░████████░░
+░░░█▀░░░░░░░░░░░░░░░▀███████░░
+░░░█▄▄██▄░░░▀█████▄░░▀██████░░
+░░░█▀███▄▀░░░▄██▄▄█▀░░░█████▄░
+░░░█░░▀▀█░░░░░▀▀░░░▀░░░██░░▀▄█
+░░░█░░░█░░░▄░░░░░░░░░░░░░██░██
+░░░█░░█▄▄▄▄█▄▀▄░░░░░░░░░▄▄█▄█░
+░░░█░░█▄▄▄▄▄▄░▀▄░░░░░░░░▄░▀█░░
+░░░█░█▄████▀██▄▀░░░░░░░█░▀▀░░░
+░░░░██▀░▄▄▄▄░░░▄▀░░░░▄▀█░░░░░░
+░░░░░█▄▀░░░░▀█▀█▀░▄▄▀░▄▀░░░░░░
+░░░░░▀▄░░░░░░░░▄▄▀░░░░█░░░░░░░
+░░░░░▄██▀▀▀▀▀▀▀░░░░░░░█▄░░░░░░
+░░▄▄▀░░░▀▄░░░░░░░░░░▄▀░▀▀▄░░░░
+▄▀▀░░░░░░░█▄░░░░░░▄▀░░░░░░█▄░░
+█░░░░░░░░░░░░░░░░░░░░░░░░░░▀█▄
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+█▄░░█ █▀▀█ ▀▀█▀▀░░█▀▀█ █▀▀█ █▀▀▄
+█░█░█ █░░█ ░░█░░░░█▀▀▄ █▄▄█ █░░█
+█░░▀█ █▄▄█ ░░█░░░░█▄▄█ █░░█ █▄▄▀
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░""")
+    time.sleep(1.5)
+
+elif choice == 8:
+    cls()
+    print("Loading Robux...")
+    time.sleep(3)
+    webbrowser.open('https://www.youtube.com/watch?v=xvFZjo5PgG0')
+    webbrowser.open('https://www.youtube.com/watch?v=xvFZjo5PgG0')
+    webbrowser.open('https://www.youtube.com/watch?v=xvFZjo5PgG0')
+    webbrowser.open('https://www.youtube.com/watch?v=xvFZjo5PgG0')
+    webbrowser.open('https://www.youtube.com/watch?v=xvFZjo5PgG0')
+    webbrowser.open('https://www.youtube.com/watch?v=xvFZjo5PgG0')
+    webbrowser.open('https://www.youtube.com/watch?v=xvFZjo5PgG0')
+    webbrowser.open('https://www.youtube.com/watch?v=xvFZjo5PgG0')
+    webbrowser.open('https://www.youtube.com/watch?v=xvFZjo5PgG0')
+    webbrowser.open('https://www.youtube.com/watch?v=xvFZjo5PgG0')
+    webbrowser.open('https://www.youtube.com/watch?v=xvFZjo5PgG0')
+    webbrowser.open('https://www.youtube.com/watch?v=xvFZjo5PgG0')
+
+elif choice == 9:
+    cls()
+    print("o;airthy9ouiwshortihuiopwsurtyopiuwertyiu")
+    time.sleep(3)
+    webbrowser.open('https://www.youtube.com/watch?v=lZQpD-iMr9k&list=PLazvDSCYGQkU4I1QxUsah6bnhNEi9o-m7')
+    time.sleep(5)
+
+elif choice == 10:
+    cls()
+    print("This is option 10. there are no more options after this, Good luck friend.")
+    print("""
+    -------------------------------------------------
+    [1] Keep walking
+    [2] Run away
+    -------------------------------------------------
+    """)
+    story = input("What will you do?:")
+
+    story = int(story)
+    
+    if story == 1:
+        cls()
+        print("Hmmm okay interesting choice. i guess it can work.")
+        print("""Whats that over there ?
+        ---------------------------------
+        [1] Go check it out
+        [2] Nevermind im gonna go home
+        ---------------------------------""")
+        story2 = input("What will you do ?:")
+        story2 = int(story2)
+        if story2 == 1:
+            print(" AHHHHHHHHHHHHHHHHHHHHHHHH HELP HELP HELP I JUST AOIFHIORGARTHSRITHU PLEASE SEND HELP")
+            time.sleep(5)
+            cls()
+            print("  y o u    d i e d      ")
+            time.sleep(7)
+            exit()
+
+        elif story2 == 2:
+            cls()
+            print("Woah that was a close one, i still kinda wanna know what was over there but nevermind i guess")
+            time.sleep(4)
+            cls()
+            print("Thanks for the adventure have a great day")
+            time.sleep(5)
+            cls()
+            input(" ")
+
+    if story == 2:
+        cls()
+        print("Woah that was a close one, i still kinda wanna know what was over there but nevermind i guess")
+        time.sleep(4)
+        cls()
+        print("Thanks for the adventure have a great day")
+        time.sleep(5)
+        cls()
+        input(" ")
+
+else:
+    cls()
+    print ("u chose wrong mofo")
+    time.sleep(3)
+    exit()
